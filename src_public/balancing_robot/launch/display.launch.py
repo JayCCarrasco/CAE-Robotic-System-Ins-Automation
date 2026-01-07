dcd1d9686aec38d55a6abd9c70fe3f8c3d4de1e8
Juan Carlos del Pozo <JayCCarrasco@users.noreply.github.com>
2026-01-07 21:05:17 +0100

    Adding src_public folder. First implementation of the balancing robot URDF (model, launch, Cmake and package.xml

---
+++ src_public/balancing_robot/launch/display.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('balancing_robot')
+    urdf_path = os.path.join(pkg_share, 'description', 'urdf', 'balancing_robot.urdf')
+    rviz_config_path = os.path.join(pkg_share, 'launch', 'balancing_robot.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_path).read()}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path]
+        )
+    ])
